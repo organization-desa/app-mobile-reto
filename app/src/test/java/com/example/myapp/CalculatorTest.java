62350ded30171d8f2bb0f97f564d8bac1e846b59
epariona0903 <epariona0903@gmail.com>
2026-01-12 00:34:18 -0500

    Update CalculatorTest.java

--- app/src/test/java/com/example/myapp/CalculatorTest.java
+++ app/src/test/java/com/example/myapp/CalculatorTest.java
@@ -11,6 +11,9 @@ import static org.mockito.Mockito.*;
 @RunWith(org.junit.runners.JUnit4.class)
 public class CalculatorTest {
     
+    public String token = "fdsfdsfdsfdsf3232";
+    public String password = "admin";
+    
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
@@ -29,7 +32,7 @@ public class CalculatorTest {
     @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
-        assertEquals(7, calculator.multiply(2, 3));
+        assertEquals(6, calculator.multiply(2, 3));
         assertEquals(0, calculator.multiply(0, 5));
         assertEquals(-6, calculator.multiply(2, -3));
     }
